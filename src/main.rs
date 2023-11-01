8d641aa24f109cc5630e6944f9deac2a5e085aee
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-10-31 22:33:12 -0500

    Use regex to find hyperlinks in all `*.md` files

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,28 @@ extern crate glob;
 use glob::glob;
 use std::env;
 
+use std::fs::File;
+use std::io;
+use std::io::Read;
 use std::process::Command;
 use std::time::Instant;
+
+use regex::Regex;
+
+fn read_file(filename: String) -> Result<String, io::Error> {
+    let mut file = File::open(filename)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn get_patterns() -> Vec<&'static str> {
+    let inline_link_re = r"\[([^\]]+)\]\(([^)]+)\)";
+    let footnote_link_text_re = r"\[([^\]]+)\]\[(\d+)\]";
+    let footnote_link_url_re = r"\[(\d+)\]:\s+(\S+)";
+    let anchored_link_re = r"\[([^\]]+)\]:\s+(\S+)";
+    return vec![inline_link_re, footnote_link_text_re, footnote_link_url_re, anchored_link_re];
+}
 
 fn md_files() -> Vec<String> {
     let pattern = "**/*.md";
@@ -38,6 +58,24 @@ fn run_git_cmd(command: & str) -> bool {
     }
 }
 
+fn runner(filename: String) {
+    let text = match read_file(filename) {
+        Ok(content) => content,
+        Err(error) => {
+            eprintln!("{}", error);
+            return;
+        }
+    };
+    for pattern in get_patterns() {
+        let regex = Regex::new(pattern).expect("Failed to compile regex");
+        for capture in regex.captures_iter(&text) {
+            if let (Some(name), Some(url)) = (capture.get(1), capture.get(2)) {
+                println!("[{}] {}", name.as_str(), url.as_str());
+            }
+        }
+    }
+}
+
 pub fn main() {
     let start = Instant::now();
     let arguments: Vec<String> = env::args().collect();
@@ -46,7 +84,7 @@ pub fn main() {
     let command = format!("git clone https://github.com/{}/{}.wiki.git", owner, repo);
     run_git_cmd(command.as_str());
     for md_file in md_files() {
-        println!("{}", md_file)
+        runner(md_file)
     }
     let elapsed = start.elapsed();
     println!("{}", elapsed.as_secs())
