1137dca492b351c68d43d9025e0a7ab8861586ba
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-11-01 13:05:51 -0500

    Add default exclusions for `localhost` Exclude URL failure on exclusions Set exit code based on failure flag Bump version

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-extern crate reqwest;
 extern crate glob;
 extern crate regex;
+extern crate reqwest;
 
 use std::env;
 use std::fs::File;
 use std::io;
 use std::io::Read;
+use std::net::ToSocketAddrs;
 use std::path::Path;
 use std::process::{Command, exit};
 use std::thread;
@@ -19,6 +20,23 @@ fn read_file(filename: &str) -> Result<String, io::Error> {
     let mut content = String::new();
     file.read_to_string(&mut content)?;
     Ok(content)
+}
+
+fn get_exclusions() -> Vec<String> {
+    let mut exclusions: Vec<String> = vec!["localhost".to_string(), "127.0.0.1".to_string()];
+    let server_details = "localhost:80";
+    let server: Vec<_> = server_details
+        .to_socket_addrs()
+        .expect("Unable to resolve domain")
+        .collect();
+    for slice in server.as_slice() {
+        let server_ip = slice.ip().to_string().clone();
+        if server_ip.starts_with("::") {
+            continue;
+        }
+        exclusions.push(server_ip);
+    }
+    return exclusions;
 }
 
 fn get_patterns() -> Vec<&'static str> {
@@ -59,22 +77,29 @@ fn run_git_cmd(command: &str) -> bool {
     }
 }
 
-fn verify_url(hyperlink: (String, String)) {
+fn verify_url(hyperlink: (String, String), exclusions: Vec<String>) {
     let (text, url) = hyperlink;  // type string which doesn't implement `Copy` trait
     let resp = reqwest::blocking::get(url.clone());  // since value is moved
     if resp.is_ok() {
         return;
     }
+    for exclusion in exclusions {
+        if url.starts_with(&exclusion) {
+            println!("'{}' failed to resolve but excluded", url);
+            return;
+        }
+    }
     // without clone, value will be borrowed after move
     println!("'{}' - '{}' failed to resolve", text, url);
     println!("Setting exit code to 1");
     env::set_var("exit_code", "1");
-    // todo: this should happen only when debug is enabled
-    // if resp.is_err() {
-    //     println!("{:?}", resp.err());
-    // } else {
-    //     println!("{:?}", resp.unwrap().error_for_status());
-    // }
+    if env::var("debug") {
+        if resp.is_err() {
+            println!("{:?}", resp.err());
+        } else {
+            println!("{:?}", resp.unwrap().error_for_status());
+        }
+    }
 }
 
 fn runner(filename: &str) -> bool {
@@ -88,14 +113,18 @@ fn runner(filename: &str) -> bool {
     };
     let text = text.to_string();
     let mut threads = Vec::new();
+    let exclusions = get_exclusions();
     for pattern in get_patterns() {
         let regex = Regex::new(pattern).expect("Failed to compile regex");
         for capture in regex.captures_iter(&text) {
             if let (Some(name), Some(url)) = (capture.get(1), capture.get(2)) {
                 let name = name.as_str().to_string();
                 let url = url.as_str().to_string();
+                // Requires explicit variable assignment to avoid 'use occurs due to use in closure'
+                // Clone exclusions and pass the clone into the closure
+                let exclusions_cloned = exclusions.clone();
                 let handle = thread::spawn(move || {
-                    verify_url((name, url))
+                    verify_url((name, url), exclusions_cloned)
                 });
                 threads.push(handle);
             }
@@ -122,6 +151,11 @@ fn main() {
     let arguments: Vec<String> = env::args().collect();
     let owner = &arguments[1];
     let repo = &arguments[2];
+    let fail = &arguments[3];
+    let debug = &arguments[4];
+    env::set_var("debug", debug);
+    println!("Fail flag is set to {}", fail);
+    println!("Debug flag is set to {}", debug);
     let wiki_path = format!("{}.wiki", repo);
     let command = format!("git clone https://github.com/{}/{}.git", owner, wiki_path);
     if run_git_cmd(command.as_str()) {
@@ -136,5 +170,13 @@ fn main() {
     }
     let elapsed = start.elapsed();
     println!("Run time: {}s", elapsed.as_secs());
-    exit(get_exit_code());
+    let code = get_exit_code();
+    println!("Exit code: {}", code);
+    if code == 1 && fail == "true" {
+        println!("Setting exit code to 1");
+        exit(code);
+    } else if code == 1 {
+        println!("Setting exit code to 0, although there were errors");
+    }
+    exit(0)
 }
